0e10e383e84e0664bbf4ac4428a8c9044a28b1e1
Antonio Márquez Pérez <antonio.marquez@jumptothecloud.tech>
2024-01-12 20:41:11 +0100

    chore(projen): initial configuration

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,17 +1,44 @@
-import { awscdk } from 'projen';
+import { ReleasableCommits, awscdk } from 'projen';
+import { TrailingComma } from 'projen/lib/javascript';
+
+export const CDK_VERSION = '2.111.0';
+
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Antonio Márquez Pérez',
   authorAddress: 'antonio.marquez@jumptothecloud.tech',
-  cdkVersion: '2.1.0',
+  cdkVersion: CDK_VERSION,
   defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.0.0',
-  name: 'remote-development',
+  constructsVersion: '10.3.0',
+  jsiiVersion: '5.3.0',
+  name: '@jttc/remote-development',
   projenrcTs: true,
   repositoryUrl: 'git@github.com:JumpToTheCloud/remote-development.git',
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      trailingComma: TrailingComma.ES5,
+      singleQuote: true,
+      bracketSpacing: true,
+      semi: true,
+    },
+  },
+  release: false,
+  jestOptions: {
+    jestConfig: {
+      verbose: true,
+    },
+  },
+  releasableCommits: ReleasableCommits.featuresAndFixes(),
 
+  releaseFailureIssue: true,
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
+  devDeps: ['commitizen', 'cz-customizable'],
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
+project.addScripts({
+  commit: './node_modules/cz-customizable/standalone.js',
+});
+
 project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="@jttc/remote-development.Hello"></a>
+
+#### Initializers <a name="Initializers" id="@jttc/remote-development.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from '@jttc/remote-development'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#@jttc/remote-development.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="@jttc/remote-development.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
